95bb77c2bfa79a3fbe665dacbad97104b287ffd4
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-11-08 14:38:24 -0300

    Predicate

--- src/FunctionExample.java
+++ src/FunctionExample.java
@@ -1,9 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.toList;
 
 public class FunctionExample {
 
